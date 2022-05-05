b9b405a52ff28b91f7ad05dd5f1d5221751544d2
SantoshThakare <santoshthakare50@gmail.com>
2022-05-05 19:49:22 +0530

    add data structure program using linked list operation

--- bank_counter.py
+++ bank_counter.py
@@ -1,33 +1,41 @@
-class Queques:
+class Queue:
     def __init__(self):
         self.balance = 0
-        print(" Welcome to the Deposit & Withdrawal Machine")
+        print("Welcome .....")
+        print("This is a Banking portal")
 
     def enqueue_deposit(self):
         amount = float(input("Enter amount to be Deposited: "))
         self.balance += amount
-        print("\n Amount Deposited:", amount)
+        print("\nAmount Deposited:", amount)
 
     def dequeue_withdraw(self):
         amount = float(input("Enter amount to be Withdrawn: "))
         if self.balance >= amount:
-            self.balance -= hash_map.pyamount
-            print("\n You Withdrew:", amount)
+            self.balance -= amount
+            print("\nYou Withdrew:", amount)
         else:
-            print("\n Insufficient balance  ")
+            print("\nInsufficient balance ")
 
+    def queue_display(self):
+        print("\nNet Available Balance=", self.balance)
 
-    def display(self):
-        print("\n Net Available Balance=", self.balance)
-
-
-    def _exit(self):
+    def queue_exit(self):
         exit()
 
-if __name__ == '__main__':
 
-    q = Queques()
-    q.enqueue_deposit()
-    q.dequeue_withdraw()
-    q.display()
-    q._exit()
+if __name__ == '__main__':
+    q = Queue()
+    while True:
+        print("Please Enter the option that you want to make a transaction:")
+        n = int(input(
+            " Press \n 1. Deposite Amount to the account \n 2. Withdraw Amount from the account \n "
+            "3. Display the amount \n 4. Cancel Transaction \n"))
+        if n == 1:
+            q.enqueue_deposit()
+        elif n == 2:
+            q.dequeue_withdraw()
+        elif n == 3:
+            q.queue_display()
+        elif n == 4:
+            q.queue_exit()
